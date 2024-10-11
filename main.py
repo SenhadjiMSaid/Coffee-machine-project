4d8ab400794b1f65a135843b78443af48f1e746d
SenhadjiMSaid <saidsenhadji06@gmail.com>
2024-10-12 00:50:26 +0100

    second commit

--- main.py
+++ main.py
@@ -85,5 +85,4 @@ while is_on:
             payment = process_coins()
             if is_transaction_successful(payment, drink['cost']):
                 make_coffe(choice, drink['ingredients'])
-
-
+                print("try ")
